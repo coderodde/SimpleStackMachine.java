9492fb8f60522041634c7b5c9f6b9ea1644cb297
Rodion Efremov <coderodd3@yandex.com>
2025-07-08 20:21:43 +0300

    ...

--- src/main/java/io/github/coderodde/simple/stack/machine/Operation.java
+++ src/main/java/io/github/coderodde/simple/stack/machine/Operation.java
@@ -1,5 +1,22 @@
 package io.github.coderodde.simple.stack.machine;
 
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.AddInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.CompareInstructionImplementation;    
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.ConstInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.DivideInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.DuplicateInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.HaltInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.InstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.LoadInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.ModuloInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.MultiplyInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.NopInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.PopInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.PushInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.StoreInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.SubInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.SwapInstructionImplementation;
+import io.github.coderodde.simple.stack.machine.SimpleStackMachine.UnconditionalJumpInstructionImplementation;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,56 +29,66 @@ import java.util.Map;
  */
 public enum Operation {
     
-    NOP   ("nop",   (byte) 0x0), // No-operation.
-    PUSH  ("push",  (byte) 0x1), // Push a value to the stack.
-    POP   ("pop",   (byte) 0x2), // Pop the stack.
-    CONST ("const", (byte) 0x0), // Push a value to the stack. Effectively, POP.
-    LOAD  ("load",  (byte) 0x4), // Pushes a value from the tape to the stack.
-    STORE ("store", (byte) 0x5), // Pops the stack and stores the popped value 
+    NOP   ("nop"  , (byte) 0x0, new NopInstructionImplementation()),
+    PUSH  ("push" , (byte) 0x1, new PushInstructionImplementation()), // Push a value to the stack.
+    POP   ("pop"  , (byte) 0x2, new PopInstructionImplementation()), // Pop the stack.
+    CONST ("const", (byte) 0x0, new ConstInstructionImplementation()), // Push a value to the stack. Effectively, POP.
+    LOAD  ("load" , (byte) 0x4, new LoadInstructionImplementation()), // Pushes a value from the tape to the stack.
+    STORE ("store", (byte) 0x5, new StoreInstructionImplementation()), // Pops the stack and stores the popped value 
                                  // at a given address of the tape.
     
-    ADD  ("add",  (byte) 0x6), // Pops two values from the stack, and pushes the
-                               // sum of two to the stack.
-    SUB  ("sub",  (byte) 0x7), // Just like "add" but stores the difference
-                               // between the top most number and the second
-                               // top most number.
-    MUL  ("mul",  (byte) 0x8), // Multiplies the two topmost numbers.
-    DIV  ("div",  (byte) 0x9), // Divides the two numbers. Top most divided by
-                               // the second top most.
-    MOD  ("mod",  (byte) 0xa), // The modulo operation, a % b, where a is the
-                               // top most number and b is under a. (Top is the
-                               // highest.)
-    CALL ("call", (byte) 0xb), // Procedure call.
-    RET  ("ret",  (byte) 0xc), // Procedure exit request.
-    DUP  ("dup",  (byte) 0xd), // Duplicates the stack.
-    SWAP ("swap", (byte) 0xe), // Swaps the two top most numbers in the stack.
+    ADD  ("add", (byte) 0x6, new AddInstructionImplementation()), // Pops two values from the stack, and pushes the
+                              // sum of two to the stack.
+    SUB  ("sub", (byte) 0x7, new SubInstructionImplementation()), // Just like "add" but stores the difference
+                              // between the top most number and the second
+                              // top most number.
+    MUL  ("mul", (byte) 0x8, new MultiplyInstructionImplementation()), // Multiplies the two topmost numbers.
+    DIV  ("div", (byte) 0x9, new DivideInstructionImplementation()), // Divides the two numbers. Top most divided by
+                              // the second top most.
+    MOD  ("mod", (byte) 0xa, new ModuloInstructionImplementation()), // The modulo operation, a % b, where a is the
+                              // top most number and b is under a. (Top is the
+                              // highest.)
     
-    CMP ("cmp", (byte) 0x0f), // Compares the two top most numbers. Sets a
+    CALL ("call", (byte) 0xb), // Procedure call.
+    RET  ("ret" , (byte) 0xc), // Procedure exit request.
+    DUP  ("dup" , (byte) 0xd, new DuplicateInstructionImplementation()), // Duplicates the stack.
+    SWAP ("swap", (byte) 0xe, new SwapInstructionImplementation()), // Swaps the two top most numbers in the stack.
+    
+    CMP ("cmp", (byte) 0x0f, new CompareInstructionImplementation()), // Compares the two top most numbers. Sets a
                               // status flag.
-    JMP ("jmp", (byte) 0x10), // Unconditional jump.
-    JZ  ("jz",  (byte) 0x11), // Jump if zero flag is set.
+    JMP ("jmp", (byte) 0x10, new UnconditionalJumpInstructionImplementation()), // Unconditional jump.
+    JZ  ("jz" , (byte) 0x11), // Jump if zero flag is set.
     JNZ ("jnz", (byte) 0x12), // Jump if zero flag is not set.
     JBZ ("jbz", (byte) 0x13), // Jump if the "below zero" flag is set.
     JAZ ("jaz", (byte) 0x14), // Jump if the "above zero" flag is set.
-    JL  ("jL",  (byte) 0xf0), // Jump if the 
+    JL  ("jl" , (byte) 0xf0), // Jump if the less than relation.
+    JLE ("jle", (byte) 0xf1), // Jump if the less than or equal relation.
+    JE  ("je" , (byte) 0xf2), // Jump if equal.
+    JNE ("jne", (byte) 0xf3), // Jump if not equal.
+    JA  ("ja" , (byte) 0xf4), // Jump if above.
+    JAE ("jae", (byte) 0xf5), // Jump if above or equal.
     
     PRINT_INT    ("iout", (byte) 0x15), // Prints the top most number.
     PRINT_STRING ("sout", (byte) 0x16), // Prints the string with bytes on the 
                                         // stack.
-    READ_INT     ("iin",  (byte) 0x17), // Read a single number and push it on 
+    READ_INT     ("iin", (byte) 0x17), // Read a single number and push it on 
                                         // the stack.
-    READ_STRING  ("sin",  (byte) 0x18); // Reads a null-terminated string and
+    READ_STRING  ("sin", (byte) 0x18), // Reads a null-terminated string and
                                         // stores its bytes in the top of the 
                                         // stack.
+    HALT ("halt", (byte) 0xff, new HaltInstructionImplementation()); // Halts the machine.
     
     private static final Map<String, Operation> mapOperationNameToOperationEnum 
             = new HashMap<>();
     
     private final byte opcodeByte;
+    private final InstructionImplementation impl;
     
     private Operation(final String name, 
-                      final byte opcodeByte) {
+                      final byte opcodeByte,
+                      final InstructionImplementation impl) {
         this.opcodeByte = opcodeByte;
+        this.impl = impl;
     }
     
     public Operation getOperation(final String operationName) {
--- src/main/java/io/github/coderodde/simple/stack/machine/SimpleStackMachine.java
+++ src/main/java/io/github/coderodde/simple/stack/machine/SimpleStackMachine.java
@@ -3,6 +3,8 @@ package io.github.coderodde.simple.stack.machine;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Objects;
+import java.util.Scanner;
+import java.util.function.BinaryOperator;
 
 /**
  * This class implements the actual stack virtual machine.
@@ -12,21 +14,325 @@ import java.util.Objects;
  */
 public class SimpleStackMachine {
     
+    public abstract class InstructionImplementation {
+        
+        private SimpleStackMachine machine;
+        
+        public void setMachine(final SimpleStackMachine machine) {
+            this.machine = machine;
+        } 
+        
+        public abstract void execute(final SimpleStackMachine machine);
+    }
+    
+    public static final class NopInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1);
+            machine.advanceInstructionPointer();
+        }
+    }
+    
+    public static class PushInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1 + Long.BYTES);
+            machine.advanceInstructionPointer();
+            
+            final long number = 
+                    machine.readNumberFromTape(machine.instructionPointer);
+            
+            machine.stack.push(number);
+        }
+    }
+    
+    public static final class PopInstructionImplementation 
+            extends InstructionImplementation {
+
+        @Override
+        public void execute(SimpleStackMachine machine) {
+            machine.checkReserve(1);
+            machine.advanceInstructionPointer();
+            machine.stack.pop();
+        }
+    }
+    
+    public static final class LoadInstructionImplementation 
+            extends InstructionImplementation {
+
+        @Override
+        public void execute(SimpleStackMachine machine) {
+            machine.checkReserve(1 + Long.BYTES);
+            machine.advanceInstructionPointer();
+            
+            final long address = 
+                    machine.readNumberFromTape(machine.instructionPointer);
+            
+            final long datum = machine.tape[(int) address];
+            
+            machine.stack.push(datum);
+        }
+    }
+    
+    public static final class StoreInstructionImplementation 
+            extends InstructionImplementation {
+
+        @Override
+        public void execute(SimpleStackMachine machine) {
+            machine.checkReserve(1 + 2 * Long.BYTES);
+            machine.advanceInstructionPointer();
+            
+            final long address = 
+                    machine.readNumberFromTape(machine.instructionPointer);
+            
+            machine.advanceInstructionPointer(Long.BYTES);
+            
+            final long datum = machine.readNumberFromTape(machine.instructionPointer);
+            
+            machine.writeNumberToTape((int) address, datum);
+            machine.advanceInstructionPointer(Long.BYTES);
+        }
+    }
+    
     /**
+     * Delegates to the pop operation.
+     */
+    public static final class ConstInstructionImplementation 
+            extends PushInstructionImplementation {
+        
+    }
+    
+    public static class BinaryArithmeticInstructionImplementation 
+            extends InstructionImplementation {
+        
+        private final BinaryOperator<Long> func;
+        
+        public BinaryArithmeticInstructionImplementation(
+                final BinaryOperator<Long> func) {
+            this.func = func;
+        }
+
+        @Override
+        public void execute(SimpleStackMachine machine) {
+            machine.checkReserve(1 + 2 * Long.BYTES);
+            machine.requireStackSize(2);
+            machine.advanceInstructionPointer();
+            
+            final long number1 = 
+                    machine.readNumberFromTape(machine.instructionPointer);
+            
+            final long number2 = 
+                    machine.readNumberFromTape(
+                            machine.instructionPointer + Long.BYTES);
+            
+            machine.stack.pop();
+            machine.stack.pop();
+            machine.stack.push(func.apply(number1, number2));
+            machine.advanceInstructionPointer(Long.BYTES * 2);
+        }
+    }
+    
+    public static final class AddInstructionImplementation 
+            extends BinaryArithmeticInstructionImplementation {
+
+        public AddInstructionImplementation() {
+            super((n1, n2) -> { return n1 + n2; });
+        }
+    }
+    
+    public static final class SubInstructionImplementation 
+            extends BinaryArithmeticInstructionImplementation {
+
+        public SubInstructionImplementation() {
+            super((n1, n2) -> { return n1 - n2; });
+        }
+    }
+    
+    public static final class MultiplyInstructionImplementation 
+            extends BinaryArithmeticInstructionImplementation {
+
+        public MultiplyInstructionImplementation() {
+            super((n1, n2) -> { return n1 * n2; });
+        }
+    }
+    
+    public static final class DivideInstructionImplementation 
+            extends BinaryArithmeticInstructionImplementation {
+
+        public DivideInstructionImplementation() {
+            super((n1, n2) -> { return n1 / n2; });
+        }
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            try {
+                super.execute(machine);
+            } catch (final ArithmeticException ex) {
+                throw new StackMachineException(ex.getMessage());
+            }
+        }
+    }
+    
+    public static final class ModuloInstructionImplementation 
+            extends BinaryArithmeticInstructionImplementation {
+
+        public ModuloInstructionImplementation() {
+            super((n1, n2) -> { return n1 % n2; });
+        }
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            try {
+                super.execute(machine);
+            } catch (final ArithmeticException ex) {
+                throw new StackMachineException(ex.getMessage());
+            }
+        }
+    }
+    
+    public static final class DuplicateInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1);
+            machine.requireStackSize(1);
+            machine.stack.push(machine.stack.element());
+            machine.advanceInstructionPointer();
+        }
+    }
+    
+    public static final class SwapInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1);
+            machine.requireStackSize(2);
+            final long number1 = machine.stack.pop();
+            final long number2 = machine.stack.pop();
+            machine.stack.push(number1);
+            machine.stack.push(number2);
+        }
+    }
+    
+    public static final class CompareInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1);
+            machine.requireStackSize(2);
+            final long number1 = machine.stack.pop();
+            final long number2 = machine.stack.pop();
+            final int cmp = Long.compare(number1, 
+                                         number2);
+            
+            if (cmp < 0) {
+                machine.flags.belowFlag = true;
+            } else if (cmp > 0) {
+                machine.flags.aboveFlag = true;
+            } else {
+                machine.flags.equalFlag = true;
+            }
+        }
+    }
+    
+    public static final class UnconditionalJumpInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1 + Long.BYTES);
+            machine.advanceInstructionPointer();
+            final int jumpAddress = 
+                    (int) machine.readNumberFromTape(
+                               machine.instructionPointer);
+            
+            machine.checkReserve(jumpAddress);
+            machine.instructionPointer = jumpAddress;
+        }
+    }
+    
+    public static final class UnconditionalJumpInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1 + Long.BYTES);
+            machine.advanceInstructionPointer();
+            final int jumpAddress = 
+                    (int) machine.readNumberFromTape(
+                               machine.instructionPointer);
+            
+            machine.checkReserve(jumpAddress);
+            machine.instructionPointer = jumpAddress;
+        }
+    }
+    
+    public static final class HaltInstructionImplementation 
+            extends InstructionImplementation {
+        
+        @Override
+        public void execute(final SimpleStackMachine machine) {
+            machine.checkReserve(1);
+            machine.haltIsRequested = true;
+        }
+    }
+    
+    private final Scanner scanner = new Scanner(System.in);
+    
+    String readString() {
+        return scanner.next();
+    }
+    
+    long readNumber() {
+        return scanner.nextLong();
+    }
+    
+    void printString(final int characterTotal, 
+                     final long[] numbers) {
+        final byte[] bytes = new byte[Long.BYTES * numbers.length];
+        
+        final StringBuilder sb = new StringBuilder();
+        
+        
+        
+        return sb.toString();
+    }
+    
+    void printNumber(final long number) {
+        System.out.println(number);
+    }
+    
+    /** 
      * The length of the memory tape in bytes. Effectively, 16 kilobytes.
      */
     private static final int TAPE_LENGTH_IN_BYTES = 16 * 1024;
-    
+   
     /**
      * This inner static class models all the processor flags.
      */
-    static final class ProcessorFlags {
+    final class ProcessorFlags {
         
         /**
          * Set to {@code true} if and only if the most previous comparison 
          * ended up with equality.
          */
         boolean equalFlag = false;
+        
+        /**
+         * Set to {@code true} if and only if the most previous comparison ended
+         * up with non-equality.
+         */
+        boolean notEqualFlag = false;
+        
+        boolean zeroFlag = false;
+        boolean notZeroFlag = false;
         
         /**
          * Set to {@code true} if and only if the most previous comparison 
@@ -43,6 +349,8 @@ public class SimpleStackMachine {
         boolean belowFlag = false;
     }
     
+    private final ProcessorFlags flags = new ProcessorFlags();
+    
     /**
      * The memory tape.
      */
@@ -53,6 +361,10 @@ public class SimpleStackMachine {
      */
     private final Deque<Long> stack = new ArrayDeque<>();
 
+    private int instructionPointer = 0;
+    
+    private boolean haltIsRequested = false;
+    
     public void execute(final byte[] programBytes) {
         Objects.requireNonNull(programBytes,
                                "The input program byte array is null.");
@@ -73,11 +385,14 @@ public class SimpleStackMachine {
                          0,
                          programBytes.length);
         
-        executeImpl();
+        while (!haltIsRequested) {
+            executeImpl();
+        }
     }
     
     private void executeImpl() {
-        
+        final byte opcode = tape[instructionPointer];
+//        final InstructionImplementation impl = Operation.
     }
     
     public static void main(String[] args) {
@@ -90,6 +405,80 @@ public class SimpleStackMachine {
                     String.format("'stack.size()' is %d, 'requestedSize' is %d",
                                   stack.size(),
                                   requestedSize));
+        }
+    }
+    
+    private long readNumberFromTape(final int address) {
+        if (address + Long.BYTES > tape.length) {
+            final String exceptionMessage = 
+                    String.format(
+                            "address(%d) + Long.BYTES(%d) > " + 
+                            "tape.length(%d) = %d", 
+                            address, 
+                            Long.BYTES, 
+                            tape.length, 
+                            Long.BYTES + tape.length);
+            
+            throw new StackMachineException(exceptionMessage);
+        }
+        
+        final long nb0 = tape[address];
+        final long nb1 = tape[address + 1] << 1;
+        final long nb2 = tape[address + 2] << 2;
+        final long nb3 = tape[address + 3] << 3;
+        final long nb4 = tape[address + 4] << 4;
+        final long nb5 = tape[address + 5] << 5;
+        final long nb6 = tape[address + 6] << 6;
+        final long nb7 = tape[address + 7] << 7;
+        
+        return nb0 |
+               nb1 |
+               nb2 |
+               nb3 |
+               nb4 |
+               nb5 |
+               nb6 |
+               nb7;
+    }
+    
+    private void writeNumberToTape(final int address, long number) {
+        
+        final byte[] bytes = new byte[Long.BYTES];
+        
+        bytes[0] = (byte) (number & 0xffL);
+        bytes[1] = (byte)((number >>= Byte.SIZE) & 0xffL);
+        bytes[2] = (byte)((number >>= Byte.SIZE) & 0xffL);
+        bytes[3] = (byte)((number >>= Byte.SIZE) & 0xffL);
+        bytes[4] = (byte)((number >>= Byte.SIZE) & 0xffL);
+        bytes[5] = (byte)((number >>= Byte.SIZE) & 0xffL);
+        bytes[6] = (byte)((number >>= Byte.SIZE) & 0xffL);
+        bytes[7] = (byte)((number >>  Byte.SIZE) & 0xffL);
+        
+        System.arraycopy(bytes, 0, tape, address, Long.BYTES);
+    }
+    
+    void advanceInstructionPointer() {
+        checkReserve(1);
+        ++instructionPointer;
+    }
+    
+    void advanceInstructionPointer(final int bytes) {
+        checkReserve(bytes);
+        instructionPointer += bytes;
+    }
+    
+    void checkReserve(int bytesToReserve) {
+        if (instructionPointer + bytesToReserve >= tape.length) {
+            final String exceptionMessage = 
+                    String.format(
+                            "instructionPointer(%d) + bytesToReserve(%d) " + 
+                                    "= %d >= tape.length(%d)",
+                            instructionPointer,
+                            bytesToReserve,
+                            bytesToReserve + instructionPointer,
+                            tape.length);
+            
+            throw new StackMachineException(exceptionMessage);
         }
     }
 }
